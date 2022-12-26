33e286fc2f76c6f3503aa010ad137fbf27808d76
Doğukan Caner <76653403+dogukancaner@users.noreply.github.com>
2022-12-26 18:55:34 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { BrowserRouter } from "react-router-dom";
 import Category from "./components/Category";
 import Navbar from "./components/Navbar";
 import Slider from "./components/Slider";
