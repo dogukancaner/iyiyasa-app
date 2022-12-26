22356626ac398acf8b812df9ebde96370a1d149f
Doğukan Caner <76653403+dogukancaner@users.noreply.github.com>
2022-12-26 15:11:57 +0300

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Logo1 from "./img/logo1.png";
 import Logo2 from "./img/logo2.png";
+import { BsSearch } from "react-icons/bs";
 import Search from "./img/search.png";
 import Fitness from "./img/fitness.png";
 import OzelSeri from "./img/ozel-seri.png";
@@ -11,40 +12,47 @@ import { BiChevronDown } from "react-icons/bi";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-center items-center p-4 border-b-2">
-      <div className="flex p-4">
-        <img className="mr-4 w-full h-full" src={Logo1} alt="" />
-        <img className="mr-4 w-full h-full" src={Logo2} alt="" />
+    <div className=" container mx-auto grid grid-cols-4 gap-4 border-b-2 pb-4">
+      <div className="flex justify-end items-center pt-2">
+        <img className="mr-4" src={Logo1} alt="" />
+        <img src={Logo2} alt="" />
       </div>
-      <div>
-        <ul className="flex justify-center items-center">
-          <li className="p-2 flex mr-4 border-b-2">
-            <input type="search" placeholder="Ara" />
-            <img className="w-full h-full " src={Search} alt="" />
-          </li>
-          <li className="flex flex-col items-center justify-center text-center mr-8">
+      <div className="flex relative justify-between items-center pt-6 m-2 p-2  ">
+        <input
+          type="text"
+          placeholder="Ara"
+          className="w-full   h-10 rounded-md border-b-2 border-black p-2 focus:outline-none focus:ring-2 focus:ring-black  mr-2"
+        />
+        <BsSearch className="absolute right-8" size={25} />
+      </div>
+      <div className="flex flex-col justify-center items-center pt-6 ">
+        <ul className="flex gap-6 ">
+          <li className="flex flex-col items-center justify-start text-center">
             <img src={Fitness} alt="" />
             <p className="text-md font-medium mt-1">Eğitmenler</p>
           </li>
-          <li className="flex flex-col items-center justify-center text-center mr-8">
+          <li className="flex flex-col items-center justify-start text-center  ">
             <img src={OzelSeri} alt="" />
             <p className="text-md font-medium mt-1">Özel Seriler</p>
           </li>
-          <li className="flex flex-col items-center justify-center text-center mr-8">
+          <li className="flex flex-col items-center justify-start text-center ">
             <img src={BanaOzel} alt="" />
             <p className="text-md font-medium mt-1">Bana Özel</p>
           </li>
-          <li className="flex flex-col items-center justify-center text-center mr-8">
+          <li className="flex flex-col items-center justify-start text-center">
             <img src={IyıBlog} alt="" />
             <p className="text-md font-medium mt-1">İyi Blog</p>
           </li>
-
-          <li className="text-center mr-14 flex">
-            <span className="mt-2">Merhaba,</span>
-            <span className=" mt-2 flex justify-center items-center w-full  ml-2">
+        </ul>
+      </div>
+      <div className="flex justify-center items-center pt-10 ">
+        <ul>
+          <li className="text-center mr-20 flex font-medium ">
+            <span className="mt-2 text-md">Merhaba,</span>
+            <button className=" mt-2 font-bold flex justify-center items-center w-full  ml-2 text-md">
               İyi Yaşa'lı
               <BiChevronDown size={25} />
-            </span>
+            </button>
           </li>
         </ul>
       </div>
