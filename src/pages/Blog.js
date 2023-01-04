4dd6d37a47021076023f586efe3d7137b796b34a
Doğukan Caner <76653403+dogukancaner@users.noreply.github.com>
2023-01-05 01:01:58 +0300

    update Blog.js

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -411,165 +411,163 @@ function IndexPage() {
         {/* <h1 className="text-center text-3xl lg:text-5xl tracking-wider text-gray-900">
             Bloglarımız
           </h1> */}
-        <h1 className="text-2xl mb-2 text-center font-bold">Bloglarımız</h1>
-        <div className="">
-          <div className="grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 gap-8 ">
-            <div className="hover:scale-95 transition duration-300 ease-in-out cursor-pointer">
-              <img
-                className="w-full "
-                src="https://cdn.tuk.dev/assets/components/111220/Blg-6/blog(1).png"
-                alt="computer"
-              />
-              <div className="py-4 px-8 w-full flex justify-between bg-gray-700">
-                <p className="text-sm text-white font-semibold tracking-wide">
-                  Bruce Wayne
+        <h1 className="text-2xl mb-2 font-bold">Bloglarımız</h1>
+        <div className="grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 gap-8 ">
+          <div className="hover:scale-95 transition duration-300 ease-in-out cursor-pointer">
+            <img
+              className="w-full "
+              src="https://cdn.tuk.dev/assets/components/111220/Blg-6/blog(1).png"
+              alt="computer"
+            />
+            <div className="py-4 px-8 w-full flex justify-between bg-gray-700">
+              <p className="text-sm text-white font-semibold tracking-wide">
+                Bruce Wayne
+              </p>
+              <p className="text-sm text-white font-semibold tracking-wide">
+                13TH Oct, 2020
+              </p>
+            </div>
+            <div className="bg-white px-10 py-6 rounded-bl-3xl rounded-br-3xl">
+              <h1 className="text-4xl text-gray-900 font-semibold tracking-wider">
+                Transactions
+              </h1>
+              <p className="text-gray-700 text-base lg:text-lg  lg:leading-8 tracking-wide mt-6 w-11/12">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Nesciunt nemo reprehenderit aliquid voluptas at quasi soluta
+                dolorum asperiores cumque perferendis nisi, quia repellendus ea
+                delectus corporis quo eius nihil debitis.
+              </p>
+              <div className="w-full mt-4 justify-end flex items-center cursor-pointer hover:scale-95 transition duration-300 ease-in-out">
+                <p className="text-base tracking-wide text-gray-500">
+                  Read more
                 </p>
-                <p className="text-sm text-white font-semibold tracking-wide">
-                  13TH Oct, 2020
-                </p>
+                <svg
+                  className="ml-3 lg:ml-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={20}
+                  height={18}
+                  viewBox="0 0 20 18"
+                  fill="none"
+                >
+                  <path
+                    d="M11.7998 1L18.9998 8.53662L11.7998 16.0732"
+                    stroke="#243763"
+                    strokeWidth={2}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M1 8.53662H19"
+                    stroke="#243763"
+                    strokeWidth={2}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
               </div>
-              <div className="bg-white px-10 py-6 rounded-bl-3xl rounded-br-3xl">
-                <h1 className="text-4xl text-gray-900 font-semibold tracking-wider">
-                  Transactions
-                </h1>
-                <p className="text-gray-700 text-base lg:text-lg  lg:leading-8 tracking-wide mt-6 w-11/12">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Nesciunt nemo reprehenderit aliquid voluptas at quasi soluta
-                  dolorum asperiores cumque perferendis nisi, quia repellendus
-                  ea delectus corporis quo eius nihil debitis.
-                </p>
-                <div className="w-full mt-4 justify-end flex items-center cursor-pointer hover:scale-95 transition duration-300 ease-in-out">
-                  <p className="text-base tracking-wide text-gray-500">
-                    Read more
+              {/* <div className="h-5 w-2" /> */}
+            </div>
+          </div>
+          <div>
+            <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-8">
+              <div className="hover:scale-95 transition duration-300 ease-in-out cursor-pointer">
+                <img
+                  className="w-full"
+                  src="https://cdn.tuk.dev/assets/components/111220/Blg-6/blog(2).png"
+                  alt="games"
+                />
+                <div className="py-2 px-4 w-full flex justify-between bg-gray-700">
+                  <p className="text-sm text-white font-semibold tracking-wide">
+                    Bruce Wayne
                   </p>
-                  <svg
-                    className="ml-3 lg:ml-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={20}
-                    height={18}
-                    viewBox="0 0 20 18"
-                    fill="none"
-                  >
-                    <path
-                      d="M11.7998 1L18.9998 8.53662L11.7998 16.0732"
-                      stroke="#243763"
-                      strokeWidth={2}
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                    <path
-                      d="M1 8.53662H19"
-                      stroke="#243763"
-                      strokeWidth={2}
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
+                  <p className="text-sm text-white font-semibold tracking-wide">
+                    13TH Oct, 2020
+                  </p>
                 </div>
-                {/* <div className="h-5 w-2" /> */}
+                <div className="bg-white px-3  lg:px-6 py-4 rounded-bl-3xl rounded-br-3xl">
+                  <h1 className="text-lg text-gray-900 font-semibold tracking-wider">
+                    Transactions
+                  </h1>
+                  <p className="text-gray-700 text-sm  lg:text-base  lg:leading-8 pr-4 tracking-wide mt-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Enim, laborum?
+                  </p>
+                </div>
+              </div>
+              <div className="hover:scale-95 transition duration-300 ease-in-out cursor-pointer">
+                <img
+                  className="w-full"
+                  src="https://cdn.tuk.dev/assets/components/111220/Blg-6/blog(3).png"
+                  alt="notes"
+                />
+                <div className="py-2 px-4 w-full flex justify-between bg-gray-700">
+                  <p className="text-sm text-white font-semibold tracking-wide">
+                    Bruce Wayne
+                  </p>
+                  <p className="text-sm text-white font-semibold tracking-wide">
+                    13TH Oct, 2020
+                  </p>
+                </div>
+                <div className="bg-white px-3  lg:px-6 py-4 rounded-bl-3xl rounded-br-3xl">
+                  <h1 className="text-lg text-gray-900 font-semibold tracking-wider">
+                    Transactions
+                  </h1>
+                  <p className="text-gray-700 text-sm  lg:text-base  lg:leading-8 pr-4 tracking-wide mt-2">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Atque doloremque, expedita commodi neque modi incidunt.
+                  </p>
+                </div>
               </div>
             </div>
-            <div>
-              <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-8">
-                <div className="hover:scale-95 transition duration-300 ease-in-out cursor-pointer">
-                  <img
-                    className="w-full"
-                    src="https://cdn.tuk.dev/assets/components/111220/Blg-6/blog(2).png"
-                    alt="games"
-                  />
-                  <div className="py-2 px-4 w-full flex justify-between bg-gray-700">
-                    <p className="text-sm text-white font-semibold tracking-wide">
-                      Bruce Wayne
-                    </p>
-                    <p className="text-sm text-white font-semibold tracking-wide">
-                      13TH Oct, 2020
-                    </p>
-                  </div>
-                  <div className="bg-white px-3  lg:px-6 py-4 rounded-bl-3xl rounded-br-3xl">
-                    <h1 className="text-lg text-gray-900 font-semibold tracking-wider">
-                      Transactions
-                    </h1>
-                    <p className="text-gray-700 text-sm  lg:text-base  lg:leading-8 pr-4 tracking-wide mt-2">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Enim, laborum?
-                    </p>
-                  </div>
+            <div className="mt-10 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-8">
+              <div className="hover:scale-95 transition duration-300 ease-in-out cursor-pointer">
+                <img
+                  className="w-full"
+                  src="https://cdn.tuk.dev/assets/components/111220/Blg-6/blog(4).png"
+                  alt="laptop"
+                />
+                <div className="py-2 px-4 w-full flex justify-between bg-gray-700">
+                  <p className="text-sm text-white font-semibold tracking-wide">
+                    Bruce Wayne
+                  </p>
+                  <p className="text-sm text-white font-semibold tracking-wide">
+                    13TH Oct, 2020
+                  </p>
                 </div>
-                <div className="hover:scale-95 transition duration-300 ease-in-out cursor-pointer">
-                  <img
-                    className="w-full"
-                    src="https://cdn.tuk.dev/assets/components/111220/Blg-6/blog(3).png"
-                    alt="notes"
-                  />
-                  <div className="py-2 px-4 w-full flex justify-between bg-gray-700">
-                    <p className="text-sm text-white font-semibold tracking-wide">
-                      Bruce Wayne
-                    </p>
-                    <p className="text-sm text-white font-semibold tracking-wide">
-                      13TH Oct, 2020
-                    </p>
-                  </div>
-                  <div className="bg-white px-3  lg:px-6 py-4 rounded-bl-3xl rounded-br-3xl">
-                    <h1 className="text-lg text-gray-900 font-semibold tracking-wider">
-                      Transactions
-                    </h1>
-                    <p className="text-gray-700 text-sm  lg:text-base  lg:leading-8 pr-4 tracking-wide mt-2">
-                      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                      Atque doloremque, expedita commodi neque modi incidunt.
-                    </p>
-                  </div>
+                <div className="bg-white px-3  lg:px-6 py-4 rounded-bl-3xl rounded-br-3xl">
+                  <h1 className="text-lg text-gray-900 font-semibold tracking-wider">
+                    Transactions
+                  </h1>
+                  <p className="text-gray-700 text-sm  lg:text-base  lg:leading-8 pr-4 tracking-wide mt-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Reprehenderit id ad iusto. Delectus vel eum enim, dolor
+                    veniam incidunt, dignissimos aliquam tempore mollitia quidem
+                    nulla?
+                  </p>
                 </div>
               </div>
-              <div className="mt-10 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-8">
-                <div className="hover:scale-95 transition duration-300 ease-in-out cursor-pointer">
-                  <img
-                    className="w-full"
-                    src="https://cdn.tuk.dev/assets/components/111220/Blg-6/blog(4).png"
-                    alt="laptop"
-                  />
-                  <div className="py-2 px-4 w-full flex justify-between bg-gray-700">
-                    <p className="text-sm text-white font-semibold tracking-wide">
-                      Bruce Wayne
-                    </p>
-                    <p className="text-sm text-white font-semibold tracking-wide">
-                      13TH Oct, 2020
-                    </p>
-                  </div>
-                  <div className="bg-white px-3  lg:px-6 py-4 rounded-bl-3xl rounded-br-3xl">
-                    <h1 className="text-lg text-gray-900 font-semibold tracking-wider">
-                      Transactions
-                    </h1>
-                    <p className="text-gray-700 text-sm  lg:text-base  lg:leading-8 pr-4 tracking-wide mt-2">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Reprehenderit id ad iusto. Delectus vel eum enim, dolor
-                      veniam incidunt, dignissimos aliquam tempore mollitia
-                      quidem nulla?
-                    </p>
-                  </div>
+              <div className="hover:scale-95 transition duration-300 ease-in-out cursor-pointer">
+                <img
+                  className="w-full"
+                  src="https://cdn.tuk.dev/assets/components/111220/Blg-6/blog(5).png"
+                  alt="worker"
+                />
+                <div className="py-2 px-4 w-full flex justify-between bg-gray-700">
+                  <p className="text-sm text-white font-semibold tracking-wide">
+                    Bruce Wayne
+                  </p>
+                  <p className="text-sm text-white font-semibold tracking-wide">
+                    13TH Oct, 2020
+                  </p>
                 </div>
-                <div className="hover:scale-95 transition duration-300 ease-in-out cursor-pointer">
-                  <img
-                    className="w-full"
-                    src="https://cdn.tuk.dev/assets/components/111220/Blg-6/blog(5).png"
-                    alt="worker"
-                  />
-                  <div className="py-2 px-4 w-full flex justify-between bg-gray-700">
-                    <p className="text-sm text-white font-semibold tracking-wide">
-                      Bruce Wayne
-                    </p>
-                    <p className="text-sm text-white font-semibold tracking-wide">
-                      13TH Oct, 2020
-                    </p>
-                  </div>
-                  <div className="bg-white px-3  lg:px-6 py-4 rounded-bl-3xl rounded-br-3xl">
-                    <h1 className="text-lg text-gray-900 font-semibold tracking-wider">
-                      Transactions
-                    </h1>
-                    <p className="text-gray-700 text-sm  lg:text-base  lg:leading-8 pr-4 tracking-wide mt-2">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Inventore, tempore nostrum autem molestias quis eveniet?
-                    </p>
-                  </div>
+                <div className="bg-white px-3  lg:px-6 py-4 rounded-bl-3xl rounded-br-3xl">
+                  <h1 className="text-lg text-gray-900 font-semibold tracking-wider">
+                    Transactions
+                  </h1>
+                  <p className="text-gray-700 text-sm  lg:text-base  lg:leading-8 pr-4 tracking-wide mt-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Inventore, tempore nostrum autem molestias quis eveniet?
+                  </p>
                 </div>
               </div>
             </div>
